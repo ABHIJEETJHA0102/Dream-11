d9cfb26b404eb0dd6d8c211063d2ffa5e3b2263f
deathgod_0809 <b22149@students.iitmandi.ac.in>
2024-11-19 18:23:43 +0530

    Added Frontend Files

---
+++ my-project/src/App.jsx
@@ -0,0 +1,11 @@
+// import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return <>
+    <h1 className="text-3xl text-blue-500 mt-4 text-center">Dream 11 Frontend</h1>
+    <p className="text-center">Welcome to Sattebaazi!</p>
+  </>;
+}
+
+export default App;
