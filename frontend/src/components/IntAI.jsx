543f6f0a33d907244dd32fd778226ce9b352be42
saurbh264 <mastersaurabh264@gmail.com>
2024-12-04 00:01:51 +0530

    Added Prediction Section

--- frontend/src/components/IntAI.jsx
+++ frontend/src/components/IntAI.jsx
@@ -4,26 +4,65 @@ const IntAI = () => {
   const [open, setOpen] = useState(false);
 
   const handleClickOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
+  const handleClose = () => {
+    setActive(-1);
+    setOpen(false);
+  };
+  const [active, setActive] = useState(-1);
   const plans = [
-    { title: "Predict Top Player", number: 1 },
-    { title: "Predict Top 5 Players ", number: 5 },
-    { title: "Predict Top 11 Players ", number: 11 },
+    { title: "Predict Top Player", number: 1, route: 'getone' },
+    { title: "Predict Top 5 Players ", number: 5 , route: 'getfive'},
+    { title: "Predict Top 11 Players ", number: 11, route: 'geteleven' },
   ];
 
+  const [predictions,setPredctions] = useState([]);
+
+  const [loading, setloading] = useState(false);
+  const UpdateDropBox = async (route,planNumber) => {
+    setloading(true);
+    setActive(planNumber);
+    const res = await fetch(`http://localhost:8000/${route}`)
+        .then((response) => response.json())
+        .then((data) => {
+            // console.log(data);
+            return data['predictions'];
+        });
+    setPredctions(res);
+
+    console.log(predictions);
+    setloading(false);
+    };
+
+
   const planCard = (plan) => {
-    return (
-      <div className="bg-[#870000] p-6 rounded-lg flex justify-between items-center shadow-md cursor-pointer">
-        <div className="mb-4">
-          <p className="font-bold text-xl text-center text-white hover:underline">{plan.title}</p>
-          <p className="text-sm text-gray-300 text-center">Predict {plan.number} Players For My Team</p>
-        </div>
-        <button className="px-4 py-2 rounded-full text-white bg-red-500 hover:bg-red-600 transition-colors">
-          Predict
-        </button>
-      </div>
-    );
+    if(loading && plan.number === active){
+        return <div className="bg-[#870000] p-6 rounded-lg flex justify-between items-center shadow-md cursor-pointer"> <p className="font-bold text-xl text-center text-white hover:underline">Loading...</p></div>    
+    }
+
+    if(predictions && predictions.length > 0 && plan.number === active){
+        return (
+            predictions.map((player) => (
+                <div key = {player.player_name} className="bg-[#870000] w-full my-1 p-6 rounded-lg flex justify-between items-center shadow-md cursor-pointer">
+                    <p className="font-bold text-xl text-center text-white hover:underline">{player.player_name}</p>
+                    <p className="text-sm text-gray-300 text-center">{player.predicted_fantasy_points} Points</p>
+                </div>
+                )
+            )
+        )
+    }
+    else if(active === -1){
+        return (
+          <div className="bg-[#870000] p-6 w-full rounded-lg flex justify-between items-center shadow-md cursor-pointer">
+            <div className="mb-4">
+              <p className="font-bold text-xl text-center text-white hover:underline">{plan.title}</p>
+              <p className="text-sm text-gray-300 text-center">Predict {plan.number} Players For My Team</p>
+            </div>
+            <button onClick = {() => UpdateDropBox(plan.route,plan.number)} className="px-4 py-2 rounded-full text-white bg-red-500 hover:bg-red-600 transition-colors">
+              Predict
+            </button>
+          </div>
+        );
+    }
   };
 
   return (
@@ -36,8 +75,8 @@ const IntAI = () => {
       </button>
 
       {open && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-          <div className="bg-gradient-to-b from-[#3d0000] to-[#870000] text-white rounded-lg shadow-lg transform transition-transform scale-95 w-[80%] md:w-[50%] py-6 px-4">
+        <div className="fixed inset-0  bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-gradient-to-b from-[#3d0000] to-[#870000] overflow-y-scroll text-white  rounded-lg shadow-lg transform transition-transform scale-95 w-[80%] max-h-4 min-h-[700px] md:w-[50%] py-6 px-4">
             <button
               className="absolute top-4 right-4 bg-red-600 text-white px-3 py-1 rounded-full hover:bg-red-500"
               onClick={handleClose}
@@ -46,10 +85,14 @@ const IntAI = () => {
             </button>
             <div className="p-4 text-lg font-semibold border-b text-center">AI Insights for Your Fantasy Team</div>
 
-            <div className="mt-4 grid grid-cols-1 sm:grid-rows-3 lg:grid-rows-3 gap-4">
-              {plans.map((plan) => (
-                <div key={plan.number}>{planCard(plan)}</div>
-              ))}
+            <div className="grid grid-cols-1 sm:grid-rows-3 lg:grid-rows-3 gap-4 py-4">
+                {plans.map((plan) => (
+                    (active === plan.number || active === -1) && (
+                    <div key={plan.number} className="flex justify-start flex-col items-start">
+                        {planCard(plan)}
+                    </div>
+                    )
+                ))}
             </div>
           </div>
         </div>
